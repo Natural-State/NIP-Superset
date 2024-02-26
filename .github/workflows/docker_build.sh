d4326dfad3b205f875015e9c408b6820cdd67eb1
gouzmi <guillaume.dupont.rm@gmail.com>
2024-02-26 15:43:29 +0100

    add docker workflow

--- .github/workflows/docker_build.sh
+++ .github/workflows/docker_build.sh
@@ -23,28 +23,28 @@ TAG="latest"
 while [[ $# -gt 0 ]]; do
     key="$1"
     case $key in
-        -p)
-            DOCKERFILE_PATH="$2"
-            shift 2
+    -p)
+        DOCKERFILE_PATH="$2"
+        shift 2
         ;;
-        -r)
-            DOCKERHUB_REPOSITORY="$2"
-            shift 2
+    -r)
+        DOCKERHUB_REPOSITORY="$2"
+        shift 2
         ;;
-        -u)
-            DOCKERHUB_USER="$2"
-            shift 2
+    -u)
+        DOCKERHUB_USER="$2"
+        shift 2
         ;;
-        -t)
-            DOCKERHUB_TOKEN="$2"
-            shift 2
+    -t)
+        DOCKERHUB_TOKEN="$2"
+        shift 2
         ;;
-        -T)
-            TAG="$2"
-            shift 2
+    -T)
+        TAG="$2"
+        shift 2
         ;;
-        *)
-            usage
+    *)
+        usage
         ;;
     esac
 done
@@ -54,7 +54,7 @@ if [ -z "$DOCKERFILE_PATH" ] || [ -z "$DOCKERHUB_REPOSITORY" ] || [ -z "$DOCKERH
     echo "Error: Missing required arguments."
     usage
 fi
-echo "ee"
+
 echo "Docker file path is ${DOCKERFILE_PATH}"
 cd $DOCKERFILE_PATH
 
@@ -62,12 +62,12 @@ docker build \
   -t "${DOCKERHUB_REPOSITORY}:${TAG}" \
   .
 
-if [ -z "${DOCKERHUB_TOKEN}" ]; then
-  # Skip if secrets aren't populated -- they're only visible for actions running in the repo (not on forks)
-  echo "Skipping Docker push"
+if [[ -z "${DOCKERHUB_TOKEN}" ]]; then
+    # Skip if secrets aren't populated -- they're only visible for actions running in the repo (not on forks)
+    echo "Skipping Docker push"
 else
-  # Login and push
-  docker logout
-  docker login --username "${DOCKERHUB_USER}" --password "${DOCKERHUB_TOKEN}"
-  docker push "${DOCKERHUB_REPOSITORY}:${TAG}"
+    # Login and push
+    docker logout
+    docker login --username "${DOCKERHUB_USER}" --password "${DOCKERHUB_TOKEN}"
+    docker push "${DOCKERHUB_REPOSITORY}:${TAG}"
 fi
